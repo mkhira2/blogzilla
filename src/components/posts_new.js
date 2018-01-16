3042ba9293cd17e12749e30f17590160a59927b2
Kenji Hirabayashi <mkhira2@gmail.com>
2018-01-15 23:48:15 -0600

    adds back end form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -38,6 +38,25 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+     const errors = {};
+
+     if (!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters!";
+     }
+
+     if (!values.categories) {
+        errors.title = "Enter some categories!";
+     }
+
+     if (!values.content) {
+        errors.title = "Enter some content!";
+     }
+
+     return errors; 
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
