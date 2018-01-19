3fb1142a5fa1e4ed8f7ee50305097072a7511c5a
Kenji Hirabayashi <mkhira2@gmail.com>
2018-01-18 23:17:29 -0600

    wires up submit and cancel buttons

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POSTS = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=GREENTEA123'
@@ -10,6 +11,15 @@ const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(values) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POSTS,
         payload: request
     };
 }
