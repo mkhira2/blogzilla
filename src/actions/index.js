08b4fad91cb98b1baec19c6dfc48a621325a4184
Kenji Hirabayashi <mkhira2@gmail.com>
2018-01-19 21:56:57 -0600

    adds navigation through callbacks

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,8 +15,9 @@ const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     };
 }
 
-export function createPost(values) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
 
     return {
         type: CREATE_POSTS,
